1ea5a08a8817285198387f3e254310d20ec46730
nick-singh <nicholas.chamansingh@gmail.com>
2014-08-21 11:29:42 -0400

    Update fileHandler.php

--- fileHandler.php
+++ fileHandler.php
@@ -9,36 +9,36 @@ class FileHandler{
 		
 		// path to admin/
 		$this_dir = dirname(__FILE__);
-
+	
 		// admin's parent dir path can be represented by admin/..
 		$parent_dir = realpath($this_dir . '/..');
-
+	
 		// concatenate the target path from the parent dir path
 		$this->path = $parent_dir . '/api/duplicates/';  		 
 	}
 
 	public static function getInstance(){
-		
-        if (!isset(static::$instance)) {
-            static::$instance = new static;
-        }
-        return static::$instance;
-    }
+	
+		if (!isset(static::$instance)) {
+		static::$instance = new static;
+		}
+		return static::$instance;
+	}
 
 
     public function createFile($userid){
-    	// timestamp when file was created
-    	$date = date('Y_m_d');
-
-		// concatenate the target path from the parent dir path
-		$target_path = $this->path.$date."_user_".$userid.".txt";		    	
-
-    	return $target_path;  		
+	// timestamp when file was created
+	$date = date('Y_m_d');
+	
+	// concatenate the target path from the parent dir path
+	$target_path = $this->path.$date."_user_".$userid.".txt";		    	
+	
+	return $target_path;  		
     }
 
 
-    public function addData($file, $data){		
-
+	public function addData($file, $data){		
+	
 		try {			
 			// file_put_contents($file,json_encode($data),FILE_APPEND);
 			file_put_contents($file,json_encode($data));
@@ -47,13 +47,13 @@ class FileHandler{
 			echo $e;
 			return false;
 		}	
-    }
+	}
 
-    public function appendData($file, $data){		
-    	$temp = json_decode(file_get_contents($file));
-    	foreach ($data as $key => $value) {
-    		array_push($temp, $value);	
-    	}    	
+	public function appendData($file, $data){		
+	    $temp = json_decode(file_get_contents($file));
+		foreach ($data as $key => $value) {
+		    	array_push($temp, $value);	
+		}    	
 		try {			
 			file_put_contents($file,json_encode($temp));			
 			return true;
@@ -61,17 +61,17 @@ class FileHandler{
 			echo $e;
 			return false;
 		}	
-    }
+	}
 
     public function openFile($file){
-    	return json_decode(file_get_contents($this->path.$file));
+	return json_decode(file_get_contents($this->path.$file));
     }
 
     public function getLast(){
 
     	$files = scandir($this->path, 1);
-		$newest_file = $files[0];				
-		return $this->path.$newest_file;
+	$newest_file = $files[0];				
+	return $this->path.$newest_file;
     }
 
     public function findFile($name){
